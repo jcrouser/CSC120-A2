3d56db1e0537d150f0384cab7baa7fe3af196f23
Lucy Auger <157738635+lauger12@users.noreply.github.com>
2024-02-03 16:38:22 -0500

    Test Commit #1-writing computer (object) attributes in computer.py

--- computer.py
+++ computer.py
@@ -1,7 +1,13 @@
 class Computer:
 
     # What attributes will it need?
-
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
