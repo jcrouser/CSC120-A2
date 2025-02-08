4ce4b6f27803ea1b1ee04689bba471e37b34c503
Your Name <you@example.com>
2025-02-08 17:53:26 -0500

    put attributes in the constructor

--- computer.py
+++ computer.py
@@ -13,8 +13,17 @@ class Computer:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processor, hdc, memory, os, year, amt: float):
+        self.description = description
+        self.processor_type = processor
+        self.hard_drive_capacity = hdc
+        self.memory = memory
+        self.operating_system = os
+        self.year_made = year
+        self.price = amt
+
+
+        #pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
 
