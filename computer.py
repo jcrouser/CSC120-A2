af98745603e3106b218c285b502a53de6723d3a7
cyang05 <ciyang@smith.edu>
2025-02-07 19:07:16 -0500

    defined attributes for comp class

--- computer.py
+++ computer.py
@@ -2,9 +2,21 @@ class Computer:
 
     # What attributes will it need?
 
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+
+    def update_os(self):
+        pass
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,38 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
+
+    inventory = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+    # What methods will you need?s
+
+    def buy(self):
+        # 1. call Comp(...) construtor to
+        # create new comp instance
+
+        # 2. call inventory.append(..) to add the 
+        # new Computer instance to the inventory
+        pass
+    
+
+    def sell(self):
+        pass
+
+    
+    def update_price(self,price:int):
+        pass
+
+    
+
+    def refurbish(self):
+        pass
+    
+
+    
