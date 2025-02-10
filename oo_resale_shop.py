2fba5eef66b15d1f3ccae7ff240142bb4a8eb358
cyang05 <ciyang@smith.edu>
2025-02-09 20:41:48 -0500

    added refurbish functions

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -25,12 +25,13 @@ class ResaleShop:
 
     
 
-    def sell(self):
-        #remove a computer from the inventory 
+    def sell(self, item_id: int):
+        if self.inventory[item_id] is not None:
+            self.inventory.pop(item_id)
+            print("Item", item_id, "sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.")
 
-
-
-        pass
 
     #Update the price of the computer 
     def update_price(self, item_id: int, new_price: int):
@@ -41,8 +42,22 @@ class ResaleShop:
         
 
 
-    def refurbish(self):
-        pass
+    def refurbish(self, item_id:int):
+        if self.inventory[item_id] is not None:
+            computer = self.inventory[item_id] # locate the computer
+        if int(computer["year_made"]) < 2000:
+            computer["price"] = 0 # too old to sell, donation only
+        elif int(computer["year_made"]) < 2012:
+            computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+        elif int(computer["year_made"]) < 2018:
+            computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+            computer["price"] = 1000 # recent stuff
+
+        if self.new_os is not None:
+            computer["operating_system"] = self.new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
     
 
     def print_inventory(self):
